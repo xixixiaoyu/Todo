198ee5ef475d21669900c495dccf2daa2bbce5d5
yunmu <1416900346@qq.com>
2026-01-17 01:27:25 +0800

    chore: 更新前端图标资源及相关配置
    将默认图标从vite.svg更改为logo.png
    更新electron构建配置中的图标引用路径

--- apps/frontend/pwa-assets.config.ts
+++ apps/frontend/pwa-assets.config.ts
@@ -2,5 +2,5 @@ import { defineConfig, minimal2023Preset } from '@vite-pwa/assets-generator/conf
 
 export default defineConfig({
   preset: minimal2023Preset,
-  images: ['public/vite.svg'],
+  images: ['public/logo.png'],
 })
